e224eabdc5464f9f0a09ba0ff802cb03fba2b45f
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-23 12:13:44 +0300

    Add PathView

--- AwardsCollection/PathView.swift
+++ AwardsCollection/PathView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct PathView: View {
     var body: some View {
-        Path { path in
+        GeometryReader { geometry in // оборачиваем чтоб зафиксировать размеры
+            let width = geometry.size.width
+            let height = geometry.size.height
+            let size = min(width, height) // размер по минимальному значению
+            let middle =  size / 2 // находим середину
+            let nearLine = size * 0.1
+            let farLine = size * 0.9
             
+            Path { path in // рисуем трапецию
+                path.move(to: CGPoint(x: middle + nearLine, y: nearLine)) // ставим первую точку
+                path.addLine(to: CGPoint(x: farLine, y: farLine)) // чертим линию ко второй
+                path.addLine(to: CGPoint(x: nearLine, y: farLine)) // третья
+                path.addLine(to: CGPoint(x: middle - nearLine, y: nearLine)) // четвертая
+            }
+            .fill(Color(red: 0.4, green: 0.4, blue: 0.4))
+            
+            Path { path in // линия на трапеции
+                path.move(to: CGPoint(x: middle, y: nearLine)) // начальная точка
+                path.addLine(to: CGPoint(x: middle, y: farLine)) // конечная
+            }
+            .stroke( // проявляем линию
+                Color.white,
+                style: StrokeStyle(
+                    lineWidth: 3,
+                    dash: [size / nearLine]
+                )
+            )
         }
     }
 }
