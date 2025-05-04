3cc835e03158bbc05f66388fba766ab7d40bc8dd
Alexander6Shevtsov <axrsvr@gmail.com>
2025-05-04 17:59:00 +0300

    Add PlaceholderForGift

---
+++ AwardsCollection/AnimationForAwards/PlaceholderForGift.swift
@@ -0,0 +1,64 @@
+//
+//  PlaceholderForGift.swift
+//  AwardsCollection
+//
+//  Created by Alexander Shevtsov on 04.05.2025.
+//
+
+import SwiftUI
+
+struct PlaceholderForGift: View {
+    let size: Double
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let size = min(geometry.size.width, geometry.size.width)
+            let middle = size / 2
+            
+            Path { path in
+                path.addArc(
+                    center: CGPoint(x: 0, y: 0),
+                    radius: middle,
+                    startAngle: .degrees(90),
+                    endAngle: .degrees(0),
+                    clockwise: true
+                )
+                path.addArc(
+                    center: CGPoint(x: size, y: 0),
+                    radius: middle,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(90),
+                    clockwise: true
+                )
+                path.addArc(
+                    center: CGPoint(x: size, y: size),
+                    radius: middle,
+                    startAngle: .degrees(270),
+                    endAngle: .degrees(180),
+                    clockwise: true
+                )
+                path.addArc(
+                    center: CGPoint(x: 0, y: size),
+                    radius: middle,
+                    startAngle: .degrees(0),
+                    endAngle: .degrees(270),
+                    clockwise: true
+                )
+            }
+            .stroke(.background, lineWidth: size / 20)
+            
+            Image(systemName: "questionmark")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.background)
+                .bold()
+                .frame(width: size, height: size)
+                .scaleEffect(0.25)
+        }
+        .scaleEffect(0.9)
+    }
+}
+
+#Preview {
+    PlaceholderForGift(size: 200)
+}
